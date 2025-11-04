bd461692275f0d760536bb9fbb41796ab3203051
David Andersson <lejonmanen@gmail.com>
2025-11-04 14:00:38 +0100

    Övning 8d

--- 8.js
+++ 8.js
@@ -15,7 +15,7 @@ for( let i = 0; i < list2.length; i++ ) {
 	if( list2[i] === 100 ) {
 		console.log('-- hittade talet 100!!')
 	}
-	
+
 	if( list2[i] === 100 ) {
 		found100 = true
 	}
@@ -23,3 +23,28 @@ for( let i = 0; i < list2.length; i++ ) {
 if( found100 ) {
 	console.log('Hittade 100')
 }
+console.log('')
+
+
+// 8d Räkna ut summa, största värdet, minsta värdet och medelvärdet för talen i listan.
+const xs = [-25, 149,  54, -21, -17, 161, -14, 0, 245, 231, 260, 216, 127,
+  134]
+
+//   Hitta största värdet i listan:
+// 1. Spara första värdet i listan i en variabel - det är störst hittills
+// 2. Titta på varje värde i listan
+// 3. Om det är större, uppdatera variabeln
+let largestSoFar = xs[0]
+let sumSoFar = xs[0]
+for( let index=1; index<xs.length; index++ ) {
+	let maybeGreater = xs[index]
+	if( maybeGreater > largestSoFar ) {
+		largestSoFar = maybeGreater
+	}
+
+	sumSoFar += xs[index]
+}
+let average = sumSoFar / xs.length
+console.log('Största talet i listan är: ' + largestSoFar)
+console.log('Summan är: ' + sumSoFar)
+console.log('Medelvärdet är: ' + average)
